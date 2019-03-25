c8a5013ee6715b84558dee3dd80e2e8617da6ecc
parijat <parijat@gmail.com>
2019-03-25 18:23:37 +0530

    Add a user defined function

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -2,6 +2,10 @@ from math import radians
 import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
 
+
+def addition(a,b):
+    return (a+b)
+
 def main():
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
