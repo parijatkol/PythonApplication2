869b2c0ab562d30811d559dc09236ab5bbdba17a
parijat <parijat@gmail.com>
2019-03-25 18:27:21 +0530

    Add another function of subtract method

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 def addition(a,b):
     return (a+b)
 
+def subtraction(a,b):
+    if(a>b):
+        return a-b
+    else:
+        return b-a
+
 def main():
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
